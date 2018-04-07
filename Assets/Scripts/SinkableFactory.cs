d964033d8c4742028deeb381362c8ce50dcbeb0e
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 19:16:33 +0300

    Create SinkableFactory

---
+++ Assets/Scripts/SinkableFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkableFactory : MonoBehaviour {
+
+    public List<Sinkable> sinkables;
+
+    public Sinkable GetSinkableByID(int id)
+    {
+        return sinkables[id];
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
