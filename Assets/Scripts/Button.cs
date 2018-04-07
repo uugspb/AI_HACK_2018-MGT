dd448ced992c350d14e792ef73228dd5c21d0860
nemestniy <tushkevich.ma@gmail.com>
2018-04-07 20:01:56 +0300

    add button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public bool click;
+
+    private void OnMouseDown()
+    {
+        click = true;
+    }
+
+    private void OnMouseUp()
+    {
+        click = false;
+    }
+
+    private void Update()
+    {
+        if (click)
+        {
+            Debug.Log("Action");
+        }
+    }
+}
