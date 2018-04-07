d3c8be795fa3d999313f78f5a7f11f8c24ee8abb
old-fritz <komarov.99@yandex.ru>
2018-04-07 23:57:22 +0300

    Divide to two types of weapons

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,10 +7,15 @@ public class Weapon : Sinkable {
     public WeaponConfig config;
     private BoxCollider2D triggerCollider;
 
+    public bool isActiveOnFall;
+    
 	// Use this for initialization
 	void Start () {
         triggerCollider = GetComponent<BoxCollider2D>();
-        triggerCollider.enabled = false;
+	    if(isActiveOnFall)
+            triggerCollider.enabled = true;
+	    else
+		    triggerCollider.enabled = false;
     }
 
     public override void Rise()
