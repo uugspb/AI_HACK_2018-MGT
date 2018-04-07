a92556eb222f85ababc413b7c3d7b3245054c4fa
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 19:17:14 +0300

    Create GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+    public SinkableFactory sinkableFactory;
+
+    public void Start()
+    {
+        instance = this;
+    }
+
+    public static Sinkable GetSinkableByID(int id)
+    {
+        return instance.sinkableFactory.GetSinkableByID(id);
+    }
+
+}
