c10828736fba0e4ef66e09c652fe03d10ae6f5f2
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 17:36:55 +0300

    Create DiebleEventChecker

---
+++ Assets/Scripts/DiebleEventChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiebleEventChecker : MonoBehaviour {
+
+    public List<Dieble> diebleList;
+
+    protected virtual void Start()
+    {
+        foreach (Dieble dieble in diebleList)
+        {
+            dieble.RegisterListener(OnInvokeEvent);
+        }
+    }
+
+    public virtual void OnInvokeEvent(Dieble diebleObject)
+    {
+        
+    }
+}
