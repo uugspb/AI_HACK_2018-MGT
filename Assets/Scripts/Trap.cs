7c071ef9c71da867afae9ce3e2d5f38b116c8b52
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 18:09:13 +0300

    Create Trap and TrapConfig

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : Dieble {
+
+    public TrapConfig config;
+
+    private float currentLiveTime;
+
+	// Use this for initialization
+	void Start () {
+        currentLiveTime = config.liveTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentLiveTime < 0)
+        {
+            Die();
+        }
+
+        currentLiveTime -= Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/TrapConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TrapConfig", menuName = "Create Trap Config")]
+
+public class TrapConfig : ScriptableObject {
+
+    public int trapID;
+    public int fishFreezeTime;
+    public float liveTime;
+}
