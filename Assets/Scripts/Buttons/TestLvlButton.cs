9e999be24e84b695602e25b42539f59d2efbf47d
old-fritz <komarov.99@yandex.ru>
2018-04-08 02:08:06 +0300

    Cheat button

---
+++ Assets/Scripts/Buttons/TestLvlButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLvlButton : Button {
+
+	protected override void OnMouseClicked()
+	{
+		
+		base.OnMouseClicked();
+		if (LevelManager.GetI().isStart)
+		{
+			LevelManager.GetI().isStart = false;
+			return;
+		}
+		foreach (Fish fish in FishGenerator.GetI().fishList)
+		{
+			fish.Die();
+		}
+
+		FishGenerator.GetI().fishCount = 0;
+	}
+}
