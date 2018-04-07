f59659b8c46b1fc3432ec5e2ec0962cb0cc146e3
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 17:36:40 +0300

    Add to Dieble event system

--- Assets/Scripts/Dieble.cs
+++ Assets/Scripts/Dieble.cs
@@ -1,12 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public class DiebleEvent : UnityEvent<Dieble>
+{
+
+}
 
 public class Dieble : MonoBehaviour {
 
-    protected virtual void Die()
+    private DiebleEvent m_diebleEvent = new DiebleEvent();
+
+    public void InvokeEvent()
+    {
+        if (m_diebleEvent != null)
+        {
+            m_diebleEvent.Invoke(this);
+        }
+    }
+
+    public void RegisterListener(UnityAction<Dieble> call)
+    {
+        if (m_diebleEvent != null)
+            m_diebleEvent.AddListener(call);
+    }
+
+    protected void Die()
     {
         PrepareToDie();
+        InvokeEvent();
         gameObject.SetActive(false);
         Destroy(gameObject);
     }
