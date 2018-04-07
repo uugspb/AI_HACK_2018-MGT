6ce66d3297d44a42f864921f31708205c7073cb2
nemestniy <tushkevich.ma@gmail.com>
2018-04-07 18:49:37 +0300

    add methods in fish

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,29 +7,55 @@ public class Fish : Dieble {
 
     [SerializeField]
     private FishConfig fishConfig;
-    private SpriteRenderer spriteRenderer;
 
     public Vector3 target;
+    public FishMind mind;
+    public double ungle;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isFishMove;
+    private int currentHP;
 
     void Start()
     {
+        ungle = 180 - Vector3.Angle(this.transform.position, target);
         spriteRenderer = this.GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = fishConfig.kindFish;
     }
 
     void Update () {
-        target = getPosition();
         this.transform.position = Vector3.MoveTowards(this.transform.position, target, fishConfig.speed * Time.deltaTime);
-        this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, this.transform.rotation.y, target.y*10);
+        this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, this.transform.rotation.y,(float)ungle);
         if (target.x-this.transform.position.x >= 0)
-            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, 0, target.y * 10);
+            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, 0, (float)ungle);
         else
-            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, 180, target.y * 10);
+            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, 180, (float)ungle);
     }
 
-    Vector3 getPosition()
+    void FishMove()
     {
-        Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
-        return Camera.main.ScreenToWorldPoint(position);
+        isFishMove = true;
+    }
+
+    void FishStay()
+    {
+        isFishMove = false;
+    }
+
+    bool IsFishMove()
+    {
+        return isFishMove;
+    }
+
+    void Hit(int hitPower)
+    {
+        currentHP -= hitPower;
+        CheckHP();
+    }
+
+    void CheckHP()
+    {
+        if (currentHP <= 0)
+            Die();
     }
 }
