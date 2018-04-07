ebdff1e8f66dd7dae9cd2cefd9cc9417a436d4f2
nemestniy <tushkevich.ma@gmail.com>
2018-04-07 16:13:36 +0300

    fix script fish

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -6,13 +6,25 @@ public class Fish : MonoBehaviour {
 
     [SerializeField]
     private FishConfig FishConfig;
+    private SpriteRenderer SpriteRenderer;
 
-    public static Vector3 target;
-    // Update is called once per frame
+    public Vector3 target;
+
+    void OnValidate()
+    {
+        SpriteRenderer = this.GetComponent<SpriteRenderer>();
+        SpriteRenderer.sprite = FishConfig.kindFish;
+    }
 
     void Update () {
-        Vector3.Lerp(this.transform.position, target, FishConfig.speed * Time.deltaTime);	
+       target = getPosition();
+       this.transform.position = Vector3.Lerp(this.transform.position, target, FishConfig.speed * Time.deltaTime);
+        this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, this.transform.rotation.y, target.y*10);
 	}
 
-
+    Vector3 getPosition()
+    {
+        Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
+        return Camera.main.ScreenToWorldPoint(position);
+    }
 }
