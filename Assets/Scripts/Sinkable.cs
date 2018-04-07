55d1d0c9b8fd453300c82e774c6049aceae7be7c
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 19:16:22 +0300

    Create Sinkable

---
+++ Assets/Scripts/Sinkable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sinkable : MonoBehaviour {
+    public int sinkableID;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
