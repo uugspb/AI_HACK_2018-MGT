e901d056daf3cc66233e323fcd426ae1f6bda23d
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-07 16:49:46 +0300

    Add Dieble class

---
+++ Assets/Scripts/Dieble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dieble : MonoBehaviour {
+
+    protected virtual void Die()
+    {
+        PrepareToDie();
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+	
+    public virtual void ReceiveKill()
+    {
+        Die();
+    }
+
+    protected virtual void PrepareToDie()
+    {
+
+    }
+}
