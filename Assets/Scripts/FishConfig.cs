85a5b83bfd08905a6e6860b5a05f2e08d1f7d598
nemestniy <tushkevich.ma@gmail.com>
2018-04-07 15:12:02 +0300

    Add Fish

---
+++ Assets/Scripts/FishConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="FishConfig", menuName ="Create FishConfig")]
+
+public class FishConfig : ScriptableObject {
+    public float probability;
+    public float speed;
+    public Sprite kindFish;
+	
+}
