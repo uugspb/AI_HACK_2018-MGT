3ec13c2b6d7ef80c7c38515b8505583ec0002dce
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-08 04:02:45 +0300

    Update Project

--- Assets/Scripts/AI/FishMind.cs
+++ Assets/Scripts/AI/FishMind.cs
@@ -13,8 +13,10 @@ public class FishMind {
 
 	public bool CheckTrap(float trapDistance, int trapID)
 	{
-        Debug.Log("Check Trap " + trapInfoList[trapID].probability);
-		return Random.Range(0, 1) < 
+        float randomValue = Random.Range(0, 1f);
+        Debug.Log("Check Trap " + trapInfoList[trapID].probability + "\nRandom Value: " + randomValue + " Total: "
+            + ((1 / Mathf.Clamp(trapDistance, 1, trapDistance)) * trapInfoList[trapID].probability));
+		return  randomValue < 
             ((1/Mathf.Clamp(trapDistance, 1, trapDistance)) * trapInfoList[trapID].probability);
 	}
 }
