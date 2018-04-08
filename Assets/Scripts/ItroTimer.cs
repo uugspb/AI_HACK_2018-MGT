aeb0eebf41b55447bca63dee130aca1a6c45240c
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-04-08 11:12:38 +0300

    Add build and update

---
+++ Assets/Scripts/ItroTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItroTimer : MonoBehaviour {
+
+    public GameObject dialog;
+    public float itroTime;
+    private float currentTime;
+    private bool isEnded = false;
+    private float currentDialogTimeForStart;
+    public float dialogTimeStart;
+    private float currentTimeDialog;
+    private float dialogTime;
+    
+
+	// Use this for initialization
+	void Start () {
+        currentTime = itroTime;
+        currentDialogTimeForStart = dialogTimeStart;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else if(!isEnded)
+        {
+            isEnded = true;
+            LevelManager.GetI().nextLevel();
+            Destroy(this);
+        }
+
+        if(currentDialogTimeForStart > 0)
+        {
+            currentDialogTimeForStart -= Time.deltaTime;
+        }
+        else
+        {
+            if (!dialog.activeSelf)
+            {
+                dialog.SetActive(true);
+                currentTimeDialog = dialogTime;
+            }
+        }
+
+	}
+}
